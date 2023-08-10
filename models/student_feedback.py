14539cdc9702d72bf748c105edfac39f43ea8c42
Rizwaan Salim <argenspin@gmail.com>
2023-08-10 12:25:47 +0530

    Counsellor is now a many2one field

--- models/student_feedback.py
+++ models/student_feedback.py
@@ -11,7 +11,7 @@ class StudentFeedback(models.Model):
 
     student_name = fields.Char('Student Name')
     mobile = fields.Char('Mobile')
-    counsellor = fields.Char('Student Counsellor')
+    counsellor = fields.Many2one('hr.employee',string='Student Counsellor')
     purpose = fields.Char('Purpose of Visit')
     rating = fields.Selection(selection=[('0','No rating'),('1','Extremely Disappointed'),('2','Unsatisfied'),('3','Informative'),('4','Satisfied'),('5','Very Satisfied')], string="Rating")
     message = fields.Text(string = "Message")
